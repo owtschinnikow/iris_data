5296a8f5c9f509cbe446c59a60320f02e540fc2a
Serg Owtschinnikow <owtschinnikow@rambler.ru>
2021-11-18 01:26:49 +0300

    add new funktions 4

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ from docutils.parsers.rst.directives.misc import Title
 
 title = 'Iris Flower Summary'
 
-# columns
+
 flowor_1 = 'Iris-setosa'
 flowor_2 = 'Iris-versicolor'
 flowor_3 = 'Iris-virginica'
@@ -16,7 +16,7 @@ metric_2_name = 'SEPAL WIDTH'
 metric_3_name = 'PETAL LENGTH'
 metric_4_name = 'PETAL WIDTH'
 
-metric_1_list = [['']*5 for i in range(3+3+7+5+7+5)]
+metric_1_list = [['']*5 for i in range(15+11)]
 metric_1_list[0][2] = title
 
 metric_1_list[1][1] = flowor_1
@@ -31,24 +31,33 @@ for i in range(1, 4):
 metric_1_list[2][4] = f' n = {number_of_flowers*3}'
 metric_1_list[4][4] = f'{number_of_flowers*3}'
 
-metric_1_list[3][0] = 'S/L [CM]'#'Sepal Lenght [CM]'
-metric_1_list[4][0] = 'N'
-metric_1_list[5][0] = 'MEAN'
-metric_1_list[6][0] = 'MIN'
-metric_1_list[7][0] = 'MAX '
-metric_1_list[8][0] = 'MEDIAN'
-metric_1_list[9][0] = 'S/D'#'STANDARD DEVIATION'
-metric_1_list[10][0] = 'S/L (%) '#'SEPAL LENGTH (%) '
+for dy in [0, 12]:
+    if dy == 0:
+        metric_1_list[3 + dy][0] = 'S/L [CM]'#'Sepal Lenght [CM]'
+        metric_1_list[10+ dy][0] = 'S/L (%) '  # 'SEPAL LENGTH (%) '
+    elif dy == 12:
+        metric_1_list[3 + dy][0] = 'S/W [CM]'#'Sepal Lenght [CM]'
+        metric_1_list[10+ dy][0] = 'S/W (%)'  # 'SEPAL LENGTH (%) '
+    metric_1_list[4 + dy][0] = 'N'
+    metric_1_list[5 + dy][0] = 'MEAN'
+    metric_1_list[6 + dy][0] = 'MIN'
+    metric_1_list[7 + dy][0] = 'MAX '
+    metric_1_list[8 + dy][0] = 'MEDIAN'
+    metric_1_list[9 + dy][0] = 'S/D'#'STANDARD DEVIATION'
+
 metric_1_list[11][0] = '< 5'
 metric_1_list[12][0] = '>=5 AND <6'
 metric_1_list[13][0] = '>=6 AND <7'
 metric_1_list[14][0] = '>= 7'
 
-
+metric_1_list[23][0] = '< 3'
+metric_1_list[24][0] = '>=3 AND <3.5'
+metric_1_list[25][0] = '>=3.5 AND <4'
 
 dataframe = pandas.read_csv('iris_data.csv', header=None)
 
 
+# ПРЕДПОЛОЖИТЕЛЬНО более быстрый расчёт по сравнению со стандартным методом
 for dx, dy in [(0,0), (1,50), (2,100)]:
     for i in range(0, number_of_flowers):
         # вычисление среднего
@@ -72,7 +81,22 @@ for dx, dy in [(0,0), (1,50), (2,100)]:
 
 # dataframe.median(axis=None, skipna=None, level=None, numeric_only=None, **kwargs)
 
-print(metric_1_list)
+# Использование функции .describe() .loc['mean'][0]
+for dx, df in [(0, 0), (1, 50), (2, 100)]:
+    df_1 = dataframe[0+df:50+df].describe()
+    print(df_1)
+    metric_1_list[16][1+dx] = "{:.1f}".format(dataframe[0+df:50+df].describe().loc['count'][1])
+    metric_1_list[17][1+dx] = "{:.1f}".format(dataframe[0+df:50+df].describe().loc['mean'][1])
+    metric_1_list[18][1+dx] = "{:.1f}".format(dataframe[0+df:50+df].describe().loc['min'][1])
+    metric_1_list[19][1+dx] = "{:.1f}".format(dataframe[0+df:50+df].describe().loc['max'][1])
+    metric_1_list[21][1+dx] = "{:.1f}".format(dataframe[0+df:50+df].describe().loc['std'][1])
+
+    metric_1_list[9][1 + dx] = "{:.1f}".format(dataframe[0 + df:50 + df].describe().loc['std'][0])
+
+# for i in range(2):
+#     metric_1_list[8][1+i] = dataframe[0:50].median()[i]
+
+
 
 df = pandas.DataFrame(metric_1_list)
 print(df)
